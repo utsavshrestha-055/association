1daecd620f794b0c42ce2f0c716f4c927682457b
utsavshrestha-055 <utsavshrestha3000@gmail.com>
2025-05-09 23:08:42 +0545

    versel deploy

--- public/index.php
+++ public/index.php
@@ -5,16 +5,23 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+// Check for maintenance mode
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+// Register Composer's autoloader
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+// Bootstrap the Laravel application
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+// Handle the incoming request
+$response = $app->handle(Request::capture());
+
+// Send the response back to the browser
+$response->send();
+
+// Ensure proper closure of the application
+$app->terminate(Request::capture(), $response);
